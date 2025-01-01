430db8ee1bd80cbf155760ad9b4bd78a6b81f81c
Moazam05 <salmanmoazam08@gmail.com>
2025-01-01 15:11:45 +0500

    images

---
+++ src/constants/images.js
@@ -0,0 +1,11 @@
+import logo from '../assets/images/logo.png';
+import onboardingOne from '../assets/images/onboarding-1.png';
+import onboardingTwo from '../assets/images/onboarding-2.png';
+import onboardingThree from '../assets/images/onboarding-3.png';
+
+export default {
+  logo,
+  onboardingOne,
+  onboardingTwo,
+  onboardingThree,
+};
