a2ef0822dc3e84e048105e0c87b05e8ddad4b69e
Moazam05 <salmanmoazam08@gmail.com>
2025-01-01 15:23:18 +0500

    colors

---
+++ src/constants/colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+  primary: '#6759FF',
+  primaryDark: '#0F1621',
+  navBg: '#18202E',
+  black: '#1A1D1F',
+  lightBlack: '#535763',
+  lightGray: '#9A9FA5',
+  white: '#FCFCFC',
+};
+
+export default Colors;
