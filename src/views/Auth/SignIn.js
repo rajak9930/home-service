47077005e472a7d15614fffa7982384dca07d53b
Moazam05 <salmanmoazam08@gmail.com>
2025-01-01 20:22:47 +0500

    styling

--- src/views/Auth/SignIn.js
+++ src/views/Auth/SignIn.js
@@ -1,49 +1,190 @@
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Animated,
+} from 'react-native';
 
 import images from '../../constants/images';
 import Colors from '../../constants/colors';
 
 const SignIn = () => {
+  const logoOpacity = useRef(new Animated.Value(0)).current;
+  const welcomeOpacity = useRef(new Animated.Value(0)).current;
+  const googleButtonOpacity = useRef(new Animated.Value(0)).current;
+  const googleButtonTranslateY = useRef(new Animated.Value(50)).current;
+  const guestButtonOpacity = useRef(new Animated.Value(0)).current;
+  const guestButtonTranslateY = useRef(new Animated.Value(50)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      // Logo fade in
+      Animated.timing(logoOpacity, {
+        toValue: 1,
+        duration: 800,
+        useNativeDriver: true,
+      }),
+      // Welcome text fade in
+      Animated.timing(welcomeOpacity, {
+        toValue: 1,
+        duration: 800,
+        useNativeDriver: true,
+      }),
+      // Google button animation
+      Animated.parallel([
+        Animated.timing(googleButtonOpacity, {
+          toValue: 1,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+        Animated.spring(googleButtonTranslateY, {
+          toValue: 0,
+          tension: 20,
+          friction: 7,
+          useNativeDriver: true,
+        }),
+      ]),
+      // Guest button animation
+      Animated.parallel([
+        Animated.timing(guestButtonOpacity, {
+          toValue: 1,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+        Animated.spring(guestButtonTranslateY, {
+          toValue: 0,
+          tension: 20,
+          friction: 7,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]).start();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const handleGoogleSignIn = () => {};
+
   return (
     <View style={styles.container}>
-      <Image source={images.authLogo} />
-      <Text>Welcome back! Glad to see you, Again</Text>
+      <Animated.View style={[styles.contentContainer, {opacity: logoOpacity}]}>
+        <Image
+          source={images.authLogo}
+          style={styles.logo}
+          resizeMode="contain"
+        />
+      </Animated.View>
 
-      <TouchableOpacity
-        onPress={handleGoogleSignIn}
-        style={styles.googleButton}
-        activeOpacity={0.8}>
-        <View style={styles.googleButtonContent}>
-          <Image
-            source={images.googleLogo}
-            style={styles.googleIcon}
-            resizeMode="contain"
-          />
-          <Text style={styles.googleButtonText}>Continue with Google</Text>
-        </View>
-      </TouchableOpacity>
+      <Animated.Text style={[styles.welcomeText, {opacity: welcomeOpacity}]}>
+        Welcome back! Glad to see you, Again
+      </Animated.Text>
 
-      <TouchableOpacity style={styles.guest}>
-        <Text>Continue as guest</Text>
-      </TouchableOpacity>
+      <Animated.View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          width: '100%',
+          opacity: googleButtonOpacity,
+          transform: [{translateY: googleButtonTranslateY}],
+        }}>
+        <TouchableOpacity
+          onPress={handleGoogleSignIn}
+          style={styles.googleButton}
+          activeOpacity={0.8}>
+          <View style={styles.googleButtonContent}>
+            <Image
+              source={images.googleLogo}
+              style={styles.googleIcon}
+              resizeMode="contain"
+            />
+            <Text style={styles.googleButtonText}>Continue with Google</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
+
+      <Animated.View
+        style={{
+          opacity: guestButtonOpacity,
+          transform: [{translateY: guestButtonTranslateY}],
+        }}>
+        <TouchableOpacity style={styles.guest}>
+          <Text style={styles.guestText}>Continue as guest</Text>
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 };
 
-export default SignIn;
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: Colors.pureWhite,
+    paddingHorizontal: 20,
+    gap: 30,
   },
-
+  contentContainer: {
+    alignItems: 'center',
+  },
+  logo: {
+    width: 200,
+    height: 100,
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: 'center',
+    color: '#333',
+  },
+  googleWrap: {
+    width: '100%',
+  },
+  googleButton: {
+    width: '100%',
+    marginTop: 20,
+    backgroundColor: '#fff',
+    paddingVertical: 12,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  googleButtonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10,
+  },
+  googleIcon: {
+    width: 24,
+    height: 24,
+  },
+  googleButtonText: {
+    fontSize: 16,
+    color: '#333',
+    fontWeight: '500',
+  },
   guest: {
     backgroundColor: '#EFEFEF',
-    padding: 10,
-    borderRadius: 5,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 10,
+    marginTop: 20,
+  },
+  guestText: {
+    fontSize: 16,
+    color: '#666',
+    fontWeight: '500',
   },
 });
+
+export default SignIn;
