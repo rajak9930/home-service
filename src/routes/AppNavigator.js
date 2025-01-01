e940c5f65b7c2dd0204e50b7d78eac47e97d9d10
Moazam05 <salmanmoazam08@gmail.com>
2025-01-01 13:11:03 +0500

    routes setup

---
+++ src/routes/AppNavigator.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Splash from '../views/Splash';
+import SignIn from '../views/Auth/SignIn';
+import Main from '../views/Main';
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Main" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
