b522a8da064ad767ea3938877763f6452a08260e
Moazam05 <salmanmoazam08@gmail.com>
2025-01-01 15:36:12 +0500

    splash screen

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,7 @@
-import logo from '../assets/images/logo.png';
-import onboardingOne from '../assets/images/onboarding-1.png';
-import onboardingTwo from '../assets/images/onboarding-2.png';
-import onboardingThree from '../assets/images/onboarding-3.png';
+import logo from '../assets/images/Logo.png';
+import onboardingOne from '../assets/images/Onboarding-1.png';
+import onboardingTwo from '../assets/images/Onboarding-2.png';
+import onboardingThree from '../assets/images/Onboarding-3.png';
 
 export default {
   logo,
--- src/views/Splash/index.js
+++ src/views/Splash/index.js
@@ -1,12 +1,44 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Image, useColorScheme} from 'react-native';
+
+import images from '../../constants/images';
+import Colors from '../../constants/colors';
 
 const Splash = () => {
+  const colorScheme = useColorScheme();
+  const [isDarkMode, setIsDarkMode] = useState(colorScheme === 'dark');
+
+  useEffect(() => {
+    setIsDarkMode(colorScheme === 'dark');
+  }, [colorScheme]);
+
   return (
-    <View>
-      <Text>Splash</Text>
+    <View
+      style={[
+        styles.container,
+        {backgroundColor: isDarkMode ? Colors.primaryDark : Colors.primary},
+      ]}>
+      <View style={styles.logoContainer}>
+        <Image source={images.logo} style={styles.logo} resizeMode="contain" />
+      </View>
     </View>
   );
 };
 
 export default Splash;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 175,
+    height: 175,
+  },
+});
