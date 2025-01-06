6d6b2fd4bb4e800a261c4c64a0d012431662632a
Moazam05 <salmanmoazam08@gmail.com>
2025-01-06 13:10:50 +0500

    add search bar

--- src/views/CategoryDetails/components/SubCategory.js
+++ src/views/CategoryDetails/components/SubCategory.js
@@ -1,18 +1,71 @@
-import {View, Text} from 'react-native';
-import React from 'react';
-import {useRoute} from '@react-navigation/native';
+import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {useNavigation, useRoute} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Colors from '../../../constants/colors';
+import SearchBar from '../../../components/SearchBar';
+import {useCustomTheme} from '../../../theme/Theme';
 
 const SubCategory = () => {
   const route = useRoute();
+  const navigation = useNavigation();
+  const theme = useCustomTheme();
+
+  const isDarkMode = theme === 'dark';
+
+  const [searchText, setSearchText] = useState('');
 
   const {category} = route.params;
 
-  console.log(category?.title);
+  console.log('title', category?.title);
   return (
-    <View>
-      <Text>SubCategory</Text>
-    </View>
+    <SafeAreaView
+      style={[
+        styles.container,
+        isDarkMode ? styles.darkContainer : styles.lightContainer,
+      ]}>
+      <View
+        style={[
+          styles.header,
+          {
+            backgroundColor: isDarkMode ? Colors.navBg : Colors.pureWhite,
+          },
+        ]}>
+        <View style={styles.searchContainer}>
+          <SearchBar
+            placeholder="Search Sub Category"
+            searchText={searchText}
+            setSearchText={setSearchText}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default SubCategory;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+  },
+  lightContainer: {
+    backgroundColor: Colors.primaryLight,
+  },
+  darkContainer: {
+    backgroundColor: Colors.primaryDark,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 12,
+  },
+  backButton: {
+    padding: 8,
+    marginRight: 8,
+  },
+  searchContainer: {
+    flex: 1,
+  },
+});
