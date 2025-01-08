90f2ac499a041062721e85a47aaa4263c05f3fd6
Moazam05 <salmanmoazam08@gmail.com>
2025-01-08 22:01:17 +0500

    styling

--- src/views/Bookings/components/Upcoming.js
+++ src/views/Bookings/components/Upcoming.js
@@ -40,70 +40,83 @@ const Upcoming = () => {
 
   return (
     <View style={[styles.wrap, isDarkMode && styles.darkWrap]}>
-      {futureBookings.map((booking, index) => (
-        <View key={index} style={[styles.card, isDarkMode && styles.darkCard]}>
-          {/* Header Section */}
-          <View style={styles.headerSection}>
-            <View style={styles.serviceIcon}>
-              <Text style={styles.serviceIconText}>W</Text>
-            </View>
-            <View style={styles.titleContainer}>
-              <Text style={[styles.title, isDarkMode && styles.darkText]}>
-                {booking.service.title}
-              </Text>
-              <Text style={styles.reference}>Reference Code: #D-571224</Text>
-            </View>
-          </View>
+      {futureBookings.map((booking, index) => {
+        const referenceCode = `${new Date().getTime()}`;
 
-          {/* Status Section */}
-          <View style={styles.row}>
-            <Text style={styles.label}>Status</Text>
-            <View style={styles.statusBadge}>
-              <Text style={styles.statusText}>Confirmed</Text>
+        return (
+          <View
+            key={index}
+            style={[styles.card, isDarkMode && styles.darkCard]}>
+            {/* Header Section */}
+            <View style={styles.headerSection}>
+              <View style={styles.serviceIcon}>
+                <Image source={images.categoryOne} style={styles.categoryOne} />
+              </View>
+              <View style={styles.titleContainer}>
+                <Text style={[styles.title, isDarkMode && styles.darkText]}>
+                  {booking.service.title}
+                </Text>
+                <Text style={styles.reference}>
+                  Reference Code: D-{referenceCode.substring(1, 7)}
+                </Text>
+              </View>
             </View>
-          </View>
 
-          {/* Schedule Section */}
-          <View style={styles.row}>
-            <View style={styles.iconWrapper}>
-              <IconOne
-                name="calendar-outline"
-                size={24}
-                color={Colors.lightGray}
-              />
+            {/* Status Section */}
+            <View style={styles.rowStatus}>
+              <Text style={styles.label}>Status</Text>
+              <View style={styles.statusBadge}>
+                <Text style={styles.statusText}>Confirmed</Text>
+              </View>
             </View>
-            <Text style={[styles.scheduleText, isDarkMode && styles.darkText]}>
-              {new Date(booking.selectedDate).toLocaleString('en-US', {
-                hour: 'numeric',
-                minute: 'numeric',
-                hour12: true,
-                month: 'short',
-                day: 'numeric',
-              })}
-            </Text>
-          </View>
 
-          {/* Provider Section */}
-          <View style={styles.row}>
-            <View style={styles.iconWrapper}>
-              <Image source={images.Upcoming} style={styles.providerIcon} />
+            {/* Schedule Section */}
+            <View style={styles.row}>
+              <View style={styles.iconWrapper}>
+                <IconOne
+                  name="calendar-outline"
+                  size={24}
+                  color={Colors.lightGray}
+                />
+              </View>
+              <View>
+                <Text
+                  style={[styles.scheduleText, isDarkMode && styles.darkText]}>
+                  {new Date(booking.selectedDate).toLocaleString('en-US', {
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    hour12: true,
+                    month: 'short',
+                    day: 'numeric',
+                  })}
+                </Text>
+                <Text style={styles.schedule}>Schedule</Text>
+              </View>
             </View>
-            <View>
-              <Text
-                style={[styles.providerName, isDarkMode && styles.darkText]}>
-                Westinghouse
-              </Text>
-              <Text style={styles.providerLabel}>Service provider</Text>
-            </View>
-          </View>
 
-          {/* Call Button */}
-          <TouchableOpacity style={styles.callButton}>
-            <IconOne name="call" size={24} color={Colors.pureWhite} />
-            <Text style={styles.callButtonText}>Call</Text>
-          </TouchableOpacity>
-        </View>
-      ))}
+            {/* Provider Section */}
+            <View style={styles.row}>
+              <View style={styles.serviceIcon}>
+                <Text style={styles.serviceIconText}>W</Text>
+              </View>
+
+              <View>
+                <Text
+                  style={[styles.providerName, isDarkMode && styles.darkText]}>
+                  Westinghouse
+                </Text>
+                <Text style={styles.schedule}>Service provider</Text>
+              </View>
+            </View>
+
+            {/* Call Button */}
+            <TouchableOpacity style={styles.callButton}>
+              <IconOne name="call" size={24} color={Colors.pureWhite} />
+              <Text style={styles.callButtonText}>Call</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      })}
     </View>
   );
 };
@@ -130,6 +143,11 @@ const styles = StyleSheet.create({
     color: '#333',
     marginBottom: 8,
   },
+  categoryOne: {
+    width: 51,
+    height: 51,
+    resizeMode: 'contain',
+  },
   subText: {
     fontSize: 14,
     color: '#666',
@@ -154,6 +172,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: '600',
   },
+  schedule: {
+    color: Colors.lightGray,
+    fontSize: 12,
+    marginTop: 2,
+  },
   card: {
     margin: 16,
     padding: 16,
@@ -166,7 +189,10 @@ const styles = StyleSheet.create({
   },
   headerSection: {
     flexDirection: 'row',
-    marginBottom: 24,
+    // marginBottom: 24,
+    borderBottomColor: '#EFEFEF',
+    borderBottomWidth: 1,
+    paddingBottom: 12,
   },
   serviceIcon: {
     width: 48,
@@ -186,14 +212,20 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   title: {
-    fontSize: 20,
+    fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 4,
+    marginBottom: 2,
     color: Colors.black,
   },
   reference: {
     color: Colors.lightGray,
-    fontSize: 14,
+    fontSize: 12,
+  },
+  rowStatus: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 16,
   },
   row: {
     flexDirection: 'row',
@@ -202,21 +234,26 @@ const styles = StyleSheet.create({
   },
   statusBadge: {
     backgroundColor: '#E8F5E9',
-    paddingHorizontal: 12,
-    paddingVertical: 6,
-    borderRadius: 16,
+    paddingHorizontal: 8,
+    paddingVertical: 4,
+    borderRadius: 4,
   },
   statusText: {
     color: '#4CAF50',
     fontWeight: '500',
+    fontSize: 12,
   },
   label: {
-    fontSize: 16,
+    fontSize: 14,
     color: Colors.lightGray,
     marginRight: 12,
   },
   iconWrapper: {
     marginRight: 12,
+    padding: 8,
+    borderRadius: 50,
+    borderColor: '#EFEFEF',
+    borderWidth: 1,
   },
   scheduleText: {
     fontSize: 16,
